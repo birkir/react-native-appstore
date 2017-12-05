7fe5fdf4efe14e513f9c50dbd4f01ea00a9d1341
Hrafn Einarsson <hrafn.einarsson@gmail.com>
2017-12-05 15:33:49 +0000

    re-use collections

--- src/screens/collections/index.js
+++ src/screens/collections/index.js
@@ -1,2 +1,1 @@
-export { default } from './Collections';
-export SellerCollectionsScreen from './SellerCollections';
+export { default, SellerCollectionsScreen } from './Collections';
---
+++ src/screens/collections/utils/sellerCollectionsHoc.js
@@ -0,0 +1,38 @@
+import { mapProps } from 'recompose';
+import get from 'lodash/get';
+
+const mapper = (props) => {
+  const apps = get(props, 'data.Seller.apps');
+
+  if (!apps) {
+    return props;
+  }
+
+  const latestApp = apps
+    .reduce((a, b) => (new Date(get(a, 'versions[0].date')) > new Date(get(b, 'versions[0].date')) ? a : b));
+  const latestRelease = {
+    id: 'latest-releases',
+    apps: [latestApp],
+    type: 'SMALL_TITLE',
+    rows: 1,
+    title: 'Latest Release',
+  };
+  const appsAndGames = {
+    id: 'apps-and-games',
+    apps,
+    type: 'SMALL_TITLE',
+    rows: 3,
+    title: 'Apps & Games',
+  };
+
+  return {
+    ...props,
+    data: {
+      ...props.data,
+      allCollections: [latestRelease, appsAndGames],
+    },
+    title: get(props, 'data.Seller.name'),
+  };
+};
+
+export default mapProps(mapper);
