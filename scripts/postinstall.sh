7238ce723fb7837c807debcf86f6acf1bcd43182
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2017-12-13 12:03:12 -0500

    Run env for travis

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -9,6 +9,23 @@ touch ./ios/Config/GeneratedInfoPlistDotEnv.h
 sh ./scripts/dotenv.sh
 
 # Pod install
-# if [ -z "$TRAVIS" ]; then
-  (cd ios; pod install; cd -)
-# fi
+(cd ios; pod install; cd -)
+
+# Setup environment
+if [ -z "$TRAVIS" ]; then
+
+  # Generate ios/AppStoreClone/GoogleService-Info.plist
+  if [ ! -z "$GEN_GOOGLESERVICE_INFO_PLIST" ]; then
+    echo $GEN_GOOGLESERVICE_INFO_PLIST | base64 --decode > ios/AppStoreClone/GoogleService-Info.plist
+  fi
+
+  # Generate ios/sentry.properties
+  if [ ! -z "$GEN_SENTRY_PROPERTIES" ]; then
+    echo $GEN_SENTRY_PROPERTIES | base64 --decode > ios/sentry.properties
+  fi
+
+  # Generate .env
+  for KEY in $(cat .env_example | sed 's/\"/\\\"/g' | sed -n 's|\(.*\)=\(.*\)|\1|p'); do
+    echo "$KEY=$(printf '%s\n' "${!KEY}")" >> .env
+  done
+fi
