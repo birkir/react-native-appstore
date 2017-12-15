a424527bba4352cf23897813d737e08ec8a8745a
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2017-12-15 13:27:01 -0500

    Updates to fastlane

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -4,19 +4,41 @@ TRAVIS_BUILD=0
 
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then
 
-  # Check last commit hash that changed ./ios folder contents
-  LAST_IOS_COMMIT=$(git log --name-status -1 ios | grep -o -E -e "[0-9a-f]{40}")
+  # Find last commit hash that changed ./ios folder contents or had [BUILD] flag in it.
+  # Find last commit that was successfully built
+  IOS_COMMIT=$(git log --name-status -1 ios | grep -o -E -e "[0-9a-f]{40}")
+  BUILD_COMMIT=$(git log -g --grep='\[BUILD\]' -1 | grep -o -E -e "[0-9a-f]{40}")
+  VERSION_COMMIT=$(git log -g --grep='\[travisbuild\]' -1 | grep -o -E -e "[0-9a-f]{40}")
+  IOS_OLDER=$(git rev-list --boundary $IOS_COMMIT..$VERSION_COMMIT)
+  BUILD_OLDER=$(git rev-list --boundary $BUILD_COMMIT..$VERSION_COMMIT)
 
-  if [[ "$TRAVIS_COMMIT" == "$LAST_IOS_COMMIT" ]]; then
+  echo "[ios] commit $IOS_COMMIT"
+  echo "[build] commit $BUILD_COMMIT"
+  echo "[version] commit $VERSION_COMMIT"
+
+  if [[ "$IOS_OLDER" == "" ]]; then
+    echo "Why? iOS folder did change in the past"
+    TRAVIS_BUILD=1
+  fi
+
+  if [[ "$BUILD_OLDER" == "" ]]; then
+    echo "Why? [BUILD] message in the past"
+    TRAVIS_BUILD=1
+  fi
+
+  if [[ "$TRAVIS_COMMIT" == "$IOS_COMMIT" ]]; then
+    echo "Why? iOS folder changed now"
+    TRAVIS_BUILD=1
+  fi
+
+  if [[ "$TRAVIS_COMMIT_MESSAGE" == *"[BUILD]"* ]]; then
+    echo "Why? [BUILD] message now"
     TRAVIS_BUILD=1
   fi
 
   if [[ "$TRAVIS_COMMIT_MESSAGE" == *"[NOBUILD]"* ]]; then
+  echo "Why? [NOBUILD] message now"
     TRAVIS_BUILD=0
-  fi
-
-  if [[ "$TRAVIS_COMMIT_MESSAGE" == *"[BUILD]"* ]]; then
-    TRAVIS_BUILD=1
   fi
 fi
 
