1fa602539fde607593f81d66aedcb47006b061a2
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2017-12-18 17:31:18 -0500

    Improve build detection

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -4,40 +4,27 @@ TRAVIS_BUILD=0
 
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then
 
-  # Find last commit hash that changed ./ios folder contents or had [BUILD] flag in it.
-  # Find last commit that was successfully built
-  IOS_COMMIT=$(git log --name-status -1 ios | grep -o -E -e "[0-9a-f]{40}")
-  BUILD_COMMIT=$(git log -g --grep='\[BUILD\]' -1 | grep -o -E -e "[0-9a-f]{40}")
-  VERSION_COMMIT=$(git log -g --grep='\[travisbuild\]' -1 | grep -o -E -e "[0-9a-f]{40}")
-  IOS_OLDER=$(git rev-list --boundary $IOS_COMMIT..$VERSION_COMMIT)
-  BUILD_OLDER=$(git rev-list --boundary $BUILD_COMMIT..$VERSION_COMMIT)
+  # Foreach commit
+  LAST_VERSION=$(git log -g --grep='\[travisbuild\]' -1 | grep -o -E -e "[0-9a-f]{40}")
+  TRIGGER_IOS=$(git rev-list $LAST_VERSION..$TRAVIS_COMMIT | xargs -L1 git diff-tree --no-commit-id --name-only -r | grep "^ios")
+  TRIGGER_BUILD="$(git rev-list $LAST_VERSION..$TRAVIS_COMMIT | xargs -L1 git rev-list --format=%B --max-count=1 | grep "\[skip ci\]")"
 
-  echo "[ios] commit $IOS_COMMIT"
-  echo "[build] commit $BUILD_COMMIT"
-  echo "[version] commit $VERSION_COMMIT"
-
-  if [[ "$IOS_OLDER" == "" ]]; then
-    echo "Why? iOS folder did change in the past"
+  if [[ "$TRIGGER_IOS" != "" ]]; then
+    echo "Why? iOS folder did change in the past:"
+    echo "$TRIGGER_IOS"
+    echo ""
     TRAVIS_BUILD=1
   fi
 
-  if [[ "$BUILD_OLDER" == "" ]]; then
-    echo "Why? [BUILD] message in the past"
-    TRAVIS_BUILD=1
-  fi
-
-  if [[ "$TRAVIS_COMMIT" == "$IOS_COMMIT" ]]; then
-    echo "Why? iOS folder changed now"
-    TRAVIS_BUILD=1
-  fi
-
-  if [[ "$TRAVIS_COMMIT_MESSAGE" == *"[BUILD]"* ]]; then
-    echo "Why? [BUILD] message now"
+  if [[ "$TRIGGER_BUILD" != "" ]]; then
+    echo "Why? [BUILD] message in the past:"
+    echo "$TRIGGER_BUILD"
+    echo ""
     TRAVIS_BUILD=1
   fi
 
   if [[ "$TRAVIS_COMMIT_MESSAGE" == *"[NOBUILD]"* ]]; then
-  echo "Why? [NOBUILD] message now"
+    echo "Why? [NOBUILD] message now"
     TRAVIS_BUILD=0
   fi
 fi
